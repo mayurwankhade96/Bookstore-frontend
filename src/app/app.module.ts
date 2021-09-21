42d9568f3e194548da38188adf057493f8b92811
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-09-21 09:30:38 +0530

    [Mayur] Add : Added signup form:sparkles:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignupComponent } from './components/signup/signup.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SlideComponent } from './components/slide/slide.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SlideComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/slide/slide.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-slide',
+  templateUrl: './slide.component.html',
+  styleUrls: ['./slide.component.scss']
+})
+export class SlideComponent implements OnInit
+{
+  signupVisibility: boolean = true;
+
+  constructor(private route: Router) { }
+
+  ngOnInit(): void
+  {
+  }
+
+  onSignup()
+  {
+    this.signupVisibility = true;
+    this.route.navigateByUrl('signup');
+  }
+
+  onLogin()
+  {
+    this.signupVisibility = true;
+  }
+}
